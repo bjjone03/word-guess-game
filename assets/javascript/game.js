95a090e6aa1ee89349cbf542c10456fc1f158790
bjjone03 <bjjone03@yahoo.com>
2018-09-03 21:42:42 -0500

    work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,72 +1,22 @@
-var $ = function (id){
-    return document.getElementById(id);
-} 
+var guess = 0;
 
-var game = [ "BEEF", "CHICKEN", "PORK", "FISH"]
-var choice = Math.floor(Math.random()*game.length);
-var answer = game[choice];
-var myLength = answer.length;
-var display=[myLength];
-var win = myLength;
-var letters = answer.split("");
-var attemptsLeft= 10;
-var output = "";
-var userLetter= "";
+var guesses = ["red", "yellow","blue","green"];
+var guessRemaining = 10;
+var guessMade = 0;
+var guessStatus = 10;
+var computer = (Math.random()*4);
+var input1 = document.querySelector("#ipbox");
+var output1 = document.querySelector("#output");
+var btn = document.querySelector("button");
 
-var setup = function()
-{
-    for (var i=0; i< answer.length; i++)
-    {
-        display[i] = "_ ";
-        output = output + display[i];
-    }
-    document.getElementById("game");
-    output = "";
-}
+btn.addEventListener("click",onMouseClick);
 
-
-var submit = function()
-{
-    output = "";
-    userLetter=$("letter").value;
-    $("letter").value ="";
-
-    for (var c = 0; c< answer.length; c++)
-    {
-        alert(letters[c]);
-        if (userLetter.toUpperCase()===letter[c])
-        {
-            display[c] = userLetter.toUpperCase();
-            win--;
-        }
-        output = output + display[c] + " ";
-    }
-
-}
-
-document.getElementById("game").innerHTML = output;
-output="";
-attemptsLeft--;
-
-if (win < 1)
-{
-    document.getElementById("guess").innerHTML = "You Win !!!";
-}
-else if (attemptsLeft < 1)
-{
-    document.getElementById("guess").innerHTML = "You Lose !!";
-}
-else
-{
-    document.getElementById("guess").innerHTML = "You have " + attemptsLeft + "guess left";
-   }
+function onMouseClick(){
+    guessRemaining--;
+    guessMade++;
+    guessStatus--;
 
 
 
-
-
-
-window.onload = function()
-{
-    setup();
+    
 }
